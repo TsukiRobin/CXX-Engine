e9688129c80551e93ef4651a23bda9193b6bd462
Robin Rogner <30783134+TsukiRobin@users.noreply.github.com>
2026-09-15 15:35:15 +0200

    Completed Chapter 24: Mouse Input
    Chapter completed.
    Added Mouse versions of our key updates.

--- src/input.h
+++ src/input.h
@@ -1,11 +1,31 @@
 #pragma once
 
+#include "SDL3/SDL_mouse.h"
 #include "SDL3/SDL_scancode.h"
 struct Input{
   const bool* keys_current;
   const bool* keys_previous;
   float* keys_held_time;
+  SDL_MouseButtonFlags mouse_current;
+  SDL_MouseButtonFlags mouse_previous;
+  float* mouse_held_time;
+  float mouse_x;
+  float mouse_y;
 };
+
+enum class MouseButtons{
+  LEFT = 0,
+  MIDDLE = 1,
+  RIGHT = 2
+};
+
+
+bool MousePressed(const Input* input, MouseButtons button);
+bool MouseReleased(const Input* input, MouseButtons button);
+bool MouseHeld(const Input* input, MouseButtons button);
+bool MouseHeld_ForTime(const Input* input, MouseButtons button, float min_length);
+void UpdateMouse(Input* input, float dt);
+
 
 bool KeyPressed(const Input* input, SDL_Scancode key);
 bool KeyHeld(const Input* input, SDL_Scancode key);
